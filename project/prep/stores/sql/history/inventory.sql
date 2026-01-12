cc827ea779710524e1e61b8ed59dd9fbe4140626
Moulisaluja <moulisaluja@gmail.com>
2026-01-12 18:54:02 +0530

    Add inventory SQL file

---
+++ project/prep/stores/sql/history/inventory.sql
@@ -0,0 +1,40 @@
+CREATE TABLE inventory (
+     inventory_date DATE ,
+	 product VARCHAR(100),
+	 inventory INTEGER,
+	 updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	 );
+	 
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Watch', 100, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Vaccum', 500, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Airconditioner', 45, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Microwave', 34, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'DVD', 6789, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'TV', 43, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Keyboard', 564, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Mice', 900, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Monitor', 435, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Mobile', 659, '2025-07-16 01:48:20');
+INSERT INTO inventory
+(inventory_date, product, inventory, updated_at)
+VALUES('2021-04-18', 'Phones', 34, '2025-07-16 01:48:20');
