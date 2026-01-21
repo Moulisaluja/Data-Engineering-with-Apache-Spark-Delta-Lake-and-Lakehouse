807aba4510bcbe4abc9c32cc221e97cdd8b8bf9c
Moulisaluja <moulisaluja@gmail.com>
2026-01-21 16:24:54 +0530

    Added reviews file

--- project/prep/ecommerce/eventhub/sendevent_1.sh
+++ project/prep/ecommerce/eventhub/sendevent_1.sh
@@ -4,10 +4,10 @@ while getopts e:n:r:s:t: option
 do
 case "${option}"
 in
-e) EVENTHUB_NAME1=${OPTARG};;
+e) EVENTHUB_NAME=${OPTARG};;
 n) EVENTHUB_NAMESPACE=${OPTARG};;
 r) RESOURCEGROUPNAME=${OPTARG};;
-s) EVENT_SUBSCRIPTION1=${OPTARG};;
+s) EVENT_SUBSCRIPTION=${OPTARG};;
 t) TOPIC=${OPTARG};;
 esac
 done
